0a919ba2871867c1cd826935cc627bda9c85f200
David Garzon <monterokdavid@gmail.com>
2020-06-17 12:38:34 -0500

    the content was modificated to responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-fu
+let size = screen.width;
+let elem = document.querySelectorAll(".img >div >div");
+let button = document.querySelector(".img >div >div >div >a");
+
+
+if (size <= 600) {
+    elem[elem.length -1].classList.add("click");
+    elem[elem.length -1].classList.remove("txt");
+    button.addEventListener("click", function () {
+        elem[elem.length -1].classList.add("click2")
+    })
+}
